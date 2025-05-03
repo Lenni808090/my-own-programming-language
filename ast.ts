05fd215d42282ac5f1a123e6e449e677da96dd40
Lenni <haselenni@gmail.com>
2025-05-03 17:10:15 +0200

    basic ast

--- ast.ts
+++ ast.ts
@@ -2,14 +2,32 @@ export type NodeType =
   | "Programm"
   | "NumericLiteral"
   | "Identifier"
-  | "BinaryExpression";
+  | "BinaryExpr";
 
+export interface Stmt {
+  kind: NodeType;
+}
 
-  export interface Stmt {
-    kind: NodeType;
-  }
+export interface Programm extends Stmt {
+  kind: "Programm";
+  body: Stmt[];
+}
 
-  export interface Programm extends Stmt {
-    kind: "Programm";
-    body: Stmt[];
-  }
+export interface Expr extends Stmt {}
+
+export interface BinaryExpr extends Expr {
+  kind: "BinaryExpr";
+  left: Expr;
+  right: Expr;
+  operator: string;
+}
+
+export interface Identifier extends Expr {
+    kind: "Identifier";
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral";
+    value: number;
+}
