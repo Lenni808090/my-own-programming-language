a048d7381a8d90f97421ffd09f3b1a31a845fcd8
Lenni <haselenni@gmail.com>
2025-05-05 19:20:40 +0200

    user defined functions

--- runtime/interpreter.ts
+++ runtime/interpreter.ts
@@ -1,51 +1,60 @@
-import { RuntimeVal, NumberVal } from "./values.ts";
+import { NumberVal, RuntimeVal } from "./values.ts";
 import {
-  BinaryExpr,
-  NumericLiteral,
-  Stmt,
-  Program,
-  Identifier,
-  VarDeclaration,
-  AssignmentExpr,
-  ObjectLiteral,
-  CallExpr,
+	AssignmentExpr,
+	BinaryExpr,
+	CallExpr,
+	FunctionDeclaration,
+	Identifier,
+	NumericLiteral,
+	ObjectLiteral,
+	Program,
+	Stmt,
+	VarDeclaration,
 } from "../frontend/ast.ts";
-import Enviroment from "./enviroment.ts";
-import { eval_programm, eval_var_declaration } from "./eval/statements.ts";
+import Environment from "./environment.ts";
 import {
-  eval_assignment,
-  eval_binary_expression,
-  eval_call_expr,
-  eval_identifier,
-  eval_object_expr,
+	eval_function_declaration,
+	eval_program,
+	eval_var_declaration,
+} from "./eval/statements.ts";
+import {
+	eval_assignment,
+	eval_binary_expr,
+	eval_call_expr,
+	eval_identifier,
+	eval_object_expr,
 } from "./eval/expressions.ts";
 
-export function evaluate(astNode: Stmt, env: Enviroment): RuntimeVal {
-  switch (astNode.kind) {
-    case "NumericLiteral":
-      return {
-        value: (astNode as NumericLiteral).value,
-        type: "number",
-      } as NumberVal;
-    case "Identifier":
-      return eval_identifier(astNode as Identifier, env);
-    case "ObjectLiteral":
-      return eval_object_expr(astNode as ObjectLiteral, env);
-    case "CallExpr":
-      return eval_call_expr(astNode as CallExpr, env);
-    case "BinaryExpr":
-      return eval_binary_expression(astNode as BinaryExpr, env);
-    case "AssignmentExpr":
-      return eval_assignment(astNode as AssignmentExpr, env);
-    case "Program":
-      return eval_programm(astNode as Program, env);
-    case "VarDeclaration":
-      return eval_var_declaration(astNode as VarDeclaration, env);
-    default:
-      console.error(
-        "this astnode has not been set up for interpretation",
-        astNode
-      );
-      Deno.exit(1);
-  }
+export function evaluate(astNode: Stmt, env: Environment): RuntimeVal {
+	switch (astNode.kind) {
+		case "NumericLiteral":
+			return {
+				value: (astNode as NumericLiteral).value,
+				type: "number",
+			} as NumberVal;
+		case "Identifier":
+			return eval_identifier(astNode as Identifier, env);
+		case "ObjectLiteral":
+			return eval_object_expr(astNode as ObjectLiteral, env);
+		case "CallExpr":
+			return eval_call_expr(astNode as CallExpr, env);
+		case "AssignmentExpr":
+			return eval_assignment(astNode as AssignmentExpr, env);
+		case "BinaryExpr":
+			return eval_binary_expr(astNode as BinaryExpr, env);
+		case "Program":
+			return eval_program(astNode as Program, env);
+		// Handle statements
+		case "VarDeclaration":
+			return eval_var_declaration(astNode as VarDeclaration, env);
+		case "FunctionDeclaration":
+			return eval_function_declaration(astNode as FunctionDeclaration, env);
+		// Handle unimplimented ast types as error.
+		default:
+			console.error(
+				"This AST Node has not yet been setup for interpretation.\n",
+				astNode
+			);
+			Deno.exit(0);
+	}
 }
--- runtime/values.ts
+++ runtime/values.ts
@@ -1,20 +1,25 @@
-import Enviroment from "./enviroment.ts";
+import Environment from "./environment.ts";
+import { Stmt } from "../frontend/ast.ts";
 
-export type ValueType = "null" | "number" | "boolean" | "object" | "native-fn";
+export type ValueType =
+    | "null"
+    | "number"
+    | "boolean"
+    | "object"
+    | "native-fn"
+    | "function";
 
-export interface RuntimeVal{
+export interface RuntimeVal {
     type: ValueType;
 }
-
 
 export interface NullVal extends RuntimeVal {
     type: "null";
     value: null;
 }
 
-export interface NumberVal extends RuntimeVal {
-    type: "number";
-    value: number;
+export function MK_NULL() {
+    return { type: "null", value: null } as NullVal;
 }
 
 export interface BooleanVal extends RuntimeVal {
@@ -22,33 +27,39 @@ export interface BooleanVal extends RuntimeVal {
     value: boolean;
 }
 
+export function MK_BOOL(b = true) {
+    return { type: "boolean", value: b } as BooleanVal;
+}
+
+export interface NumberVal extends RuntimeVal {
+    type: "number";
+    value: number;
+}
+
+export function MK_NUMBER(n = 0) {
+    return { type: "number", value: n } as NumberVal;
+}
+
 export interface ObjectVal extends RuntimeVal {
     type: "object";
     properties: Map<string, RuntimeVal>;
 }
 
+export type FunctionCall = (args: RuntimeVal[], env: Environment) => RuntimeVal;
 
-export type FunctionCall = (args: RuntimeVal[], env: Enviroment) => RuntimeVal;
-
-export interface NativeFnValue extends RuntimeVal{
+export interface NativeFnValue extends RuntimeVal {
     type: "native-fn";
     call: FunctionCall;
 }
 
-
-
-export function MK_NUMBER(n = 0) {
-    return { type: "number", value: n} as NumberVal;
-}
-
-export function MK_NULL() {
-    return { type: "null", value: null} as NullVal;
-}
-
-export function MK_BOOL(b = true) {
-    return { type: "boolean", value: b} as BooleanVal
-}
-
 export function MK_NATIVE_FN(call: FunctionCall) {
-    return { type: "native-fn", call} as NativeFnValue;
+    return { type: "native-fn", call } as NativeFnValue;
+}
+
+export interface FunctionValue extends RuntimeVal {
+    type: "function";
+    name: string;
+    parameters: string[];
+    declarationEnv: Environment;
+    body: Stmt[];
 }
